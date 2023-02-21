44f452db2c19d1c2c07b8d15315e47b4344f1fad
Lucas tecluu <57456310+2610n@users.noreply.github.com>
2023-02-21 11:54:00 -0300

    sessao usuario

--- processa_login.php
+++ processa_login.php
@@ -5,12 +5,17 @@ require_once "conexao.php";
 if(isset($_POST["btn_login"])){
    $login=mysqli_escape_string($conexao,isset($_POST["login"])?$_POST["login"]:"sem login"); 
    $senha=mysqli_escape_string($conexao,isset($_POST["senha"])?$_POST["senha"]:"sem login");
+   $senha=md5($senha);
    
    $consultar="SELECT *FROM USUARIO WHERE login ='$login' AND senha ='$senha'";
    $execultar_consulta=mysqli_query($conexao,$consultar);
    
    if(mysqli_num_rows($execultar_consulta)==1){
-      
+      $resultado=mysqli_fetch_row($execultar_consulta);
+      $_SESSION["id_usuario"]=$resultado[0];
+      $_SESSION["nome_usuario"]=$resultado[1];
+
+      header("location:home.php");
 
    }else{
       
