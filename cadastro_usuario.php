0e901f6d4240432d2ec563cf913b0c4242a0096c
Lucas tecluu <57456310+2610n@users.noreply.github.com>
2023-02-21 11:07:17 -0300

    validação de cadastro de usuario

--- cadastro_usuario.php
+++ cadastro_usuario.php
@@ -1,3 +1,4 @@
+<?php session_start();?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,6 +9,12 @@
 </head>
 <body>
         <div class="container">
+            <?php
+            if(isset($_SESSION["mensagem"])){
+                  echo $_SESSION["mensagem"];
+                  unset($_SESSION["mesagem"]);
+            }
+            ?>
                                      
                      <form action="validar_cadastro_usuario.php" method="post">
                             <label for="">Nome</label><br>
@@ -18,10 +25,11 @@
                             <input type="password" name="senha" id=""><br>
                             <label for="">Confirmar senha</label><br>
                             <input type="password" name="senha2" id=""><br>
-                            <input type="submit" value="Cadastrar">
+                            <input type="submit" value="Cadastrar" name="btn_cadastro">
 
 
                      </form>
+                     <a href="login.php">Login</a>
         </div>
 </body>
 </html>
--- validar_cadastro_usuario.php
+++ validar_cadastro_usuario.php
@@ -1,4 +1,26 @@
 <?php
-  
+  session_start();
+  require_once "conexao.php";
 
-?>
+  if(isset($_POST["btn_cadastro"])){
+
+      $nome_usu=mysqli_escape_string($conexao,isset($_POST["nome"])?$_POST["nome"]:"sem nome");
+      $senha_usu=mysqli_escape_string($conexao,isset($_POST["senha"])?$_POST["senha"]:"sem senha");
+      $login_usu=mysqli_escape_string($conexao,isset($_POST["login"])?$_POST["login"]:"sem login");
+      $senha_usu=md5($senha_usu);//criptografia de senha
+
+      $consulta_usuario="SELECT *FROM usuario WHERE  login='$login_usu'";
+      
+       $executar_consulta=mysqli_query($conexao,$consulta_usuario);
+       $linha=mysqli_num_rows($executar_consulta);
+       if($linha >0){
+            $_SESSION["mensagem"]="<p style='color:red'>este login já existe tente outro</p>";
+            header("location:cadastro_usuario.php");
+
+       }else{
+                $_SESSION["mensagem"]="<p style='color:green'>Login cadastrado com sucesso!!!</p>";
+                $inserir_dados="INSERT INTO usuario (nome,login,senha) values ('$nome_usu','$login_usu','$senha_usu')";
+                mysqli_query($conexao,$inserir_dados);
+                header("location:cadastro_usuario.php");
+       }
+  }
