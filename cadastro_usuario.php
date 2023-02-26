6b4d497157faa1e3353f9f804b0e1adaf382a9dc
Lucas tecluu <57456310+2610n@users.noreply.github.com>
2023-02-26 18:31:44 -0300

    tela de cadastro terminada

--- cadastro_usuario.php
+++ cadastro_usuario.php
@@ -5,10 +5,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/styleLogin.css">
     <title>Document</title>
 </head>
 <body>
-        <div class="container">
+        <div id="container">
+    
+        <div id="form">
             <?php
             if(isset($_SESSION["mensagem"])){
                   echo $_SESSION["mensagem"];
@@ -16,21 +19,24 @@
                   session_destroy();
             }
             ?>
-                                     
+                                <div id="titulo"><h3>Cadastro</h3></div>
+
                      <form action="validar_cadastro_usuario.php" method="post">
-                            <label for="">Nome</label><br>
-                             <input type="text" name="nome" required><br>
-                            <label for="">Login</label><br>
-                            <input type="text" name="login" required><br>
-                            <label for="">Senha</label><br>
-                            <input type="password" name="senha" id="" required><br>
-                            <label for="">Confirmar senha</label><br>
-                            <input type="password" name="senha2" id="" required><br>
-                            <input type="submit" value="Cadastrar" name="btn_cadastro">
+                        
+                             <input type="text" name="nome" required placeholder="Nome"><br><br>
+                           
+                            <input type="text" name="login" required placeholder="Login"><br><br>
+
+                            
+                            <input type="password" name="senha" id="" required placeholder="Digite a senha"><br><br>
+                            
+                            <input type="password" name="senha2" id="" required placeholder="Digite novamente a senha"><br><br>
+                            <input type="submit" value="Cadastrar" name="btn_cadastro" id="btn">
 
 
                      </form>
                      <a href="login.php">Login</a>
+        </div>
         </div>
 </body>
 </html>
