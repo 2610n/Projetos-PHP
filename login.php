99e5f4d81bbad403989662c58e083bc42689690f
Lucas tecluu <57456310+2610n@users.noreply.github.com>
2023-02-26 17:53:44 -0300

    style Login

--- login.php
+++ login.php
@@ -6,6 +6,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/styleLogin.css">
     <title>Document</title>
 </head>
 
@@ -13,24 +14,25 @@
 
     <div id="container">
         <div id="form">
+        <div id="icon"><img src="CSS/login.png" alt=""></div>
             <?php
                if(isset($_SESSION["mensagem"])){
                   echo $_SESSION["mensagem"];
                   unset($_SESSION["mensagem"]);
                   session_destroy();
                }
-            ?>
-
+            ?><br>
+                
             <form action="processa_login.php" method="post" enctype="multipart/form-data">
-
-                <label for="">Login</label><br>
-                <input type="text" name="login" required><br>
-                <label for="">Senha</label><br>
-                <input type="password" name="senha" required><br>
-                <input type="submit" value="Acessar" name="btn_login"><br>
+              
+ 
+                <input type="text" name="login" required placeholder="Login"><br><br>
+               
+                <input type="password" name="senha" required placeholder="Senha"><br><br>
+                <input type="submit" value="Acessar" name="btn_login" id="btn"><br>
           
             </form>
-            <a href="cadastro_usuario.php">Crie uma conta</a>
+            <label for=""><a href="cadastro_usuario.php">Crie uma conta</a></label>
 
         </div>
     </div>
